db9d24710c44963258192290fb92df368dd98f20
Jeppe Stockmar <jeppe@stockmar.dk>
2015-03-13 13:42:41 +0100

    Update & Upload

--- mod_upload-dropdrag.php
+++ mod_upload-dropdrag.php
@@ -29,10 +29,10 @@ $doc =& JFactory::getDocument();
 $dir = JFolder::files($folder);
 
 // 	add the stylesheet
-$doc->addStylesheet('maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css');
+$doc->addStylesheet('//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css');
 $doc->addStylesheet( JURI::root(true) . '/modules/mod_upload-dropdrag/css/style.css' );
 // 	add the jquery
-$doc->addScript('ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js');
+$doc->addScript('//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js');
 
 
 
@@ -40,13 +40,13 @@ $doc->addScript('ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js');
 $doc->addScript(JURI::root(true) . '/js/vendor/jquery.ui.widget.js');
 
 //	The Load Image plugin is included for the preview images and image resizing functionality
-$doc->addScript('blueimp.github.io/JavaScript-Load-Image/js/load-image.all.min.js');
+$doc->addScript('//blueimp.github.io/JavaScript-Load-Image/js/load-image.all.min.js');
 
 // 	The Canvas to Blob plugin is included for image resizing functionality
-$doc->addScript('blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js');
+$doc->addScript('//blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js');
 
 // 	Bootstrap JS is not required, but included for the responsive demo navigation
-$doc->addScript('netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js');
+$doc->addScript('//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js');
 
 //	The Iframe Transport is required for browsers without support for XHR file uploads
 $doc->addScript(JURI::root(true) . 'js/jquery.iframe-transport.js');
