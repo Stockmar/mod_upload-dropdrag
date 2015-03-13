742ea9439345ced7e41137be94bfd354cdd719fa
Jeppe Stockmar <jeppe@stockmar.dk>
2015-03-13 09:59:08 +0100

    upload

---
+++ tmpl/default.php
@@ -0,0 +1,16 @@
+<?php
+/*
+* Version           : 1.0
+* Created by        : Stockmar
+* Email             : info@stockmar.dk
+* Created on        : 13-03-2015
+* URL               : http://stockmar.dk
+* License           : GPLv2
+* Copyright         : 2015 stockmar.dk
+* License           : GPLv2 http://www.gnu.org/licenses/gpl-2.0.html
+*/
+defined('_JEXEC') or die;
+?>
+<section id="mod-upload-dropdrag">
+	
+</section>
